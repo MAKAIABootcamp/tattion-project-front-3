658e92ab7fe3ac65d51ca1444be3dc004ef8811c
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-04-11 21:46:41 -0500

    Create tw variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      lato: ["var(--font-lato)", ...fontFamily.sans],
+      montserrat: ["var(--font-montserrat)", ...fontFamily.sans],
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      borderRadius: {
+        large: "99999px",
+        md: "50px",
+        sm: "20px",
+        xs: "5px",
+      },
+      colors: {
+        white: "#f2f2f2",
+        "main-red": "#D01000",
+        "dark-red": "#BE1E2D",
+        "light-red": "#ED6366",
+        "light-gray": "#D9D9D9",
+        "dark-gray": "#606060",
+        "gray-black": "#242424",
       },
     },
   },
   plugins: [],
-}
+};
