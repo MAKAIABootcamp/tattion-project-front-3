60ebab8d6b7141980c34ca3eb0ed330be8758c73
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-04-13 06:04:56 -0500

    fix: works labels

--- pages/components/homepage/Development.jsx
+++ pages/components/homepage/Development.jsx
@@ -4,12 +4,17 @@ import ReactModel from "./React";
 
 const Development = () => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[3, 2, 1]} />
-      <ReactModel />
-      <OrbitControls enableZoom={false} />
-    </Canvas>
+    <>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[3, 2, 1]} />
+        <ReactModel />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+      <p className="bg-white w-[200px] h-[80px] p-5 rounded-lg text-dark-gray font-light text-sm absolute bottom-48 -right-16">
+        Technologies we used to develop this project.
+      </p>
+    </>
   );
 };
 
