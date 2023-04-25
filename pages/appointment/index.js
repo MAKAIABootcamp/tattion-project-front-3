6f13a022ef0d31b5b7580bd1c20f50142d687dc4
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-04-24 21:20:46 -0500

    Create users collection

--- pages/appointment/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from "@/layouts/MainLayout";
-import React from "react";
-
-const Website = ({ Component }) => {
-  console.log(Component);
-  return <Layout></Layout>;
-};
-
-export default Website;
--- pages/welcome/index.jsx
+++ pages/welcome/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import NextLink from "next/link";
 import Image from "next/image";
 import Layout from "@/layouts/MainLayout";
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
 
 //assets
 import logo from "@/public/landingPage/logo.png";
 import file from "@/public/welcome/file-icon.svg";
 import list from "@/public/welcome/list-icon.svg";
-import { useAuth } from "@/context/AuthContext";
 
 const Welcome = () => {
   const { currentUser } = useAuth();
@@ -18,14 +18,17 @@ const Welcome = () => {
         <Image src={logo} alt="Logo" width={300} height={280} />
 
         <article className="flex flex-col items-center gap-6 mr-4">
-          <NextLink href="/designs">
+          <Link href="/designs">
             <button className="w-full flex items-center justify-center gap-2 h-10 bg-red-600 hover:bg-red-400 transition-all drop-shadow-xl rounded-md text-white px-12">
               <Image src={file} alt="file icon" /> Quote Tattoo
             </button>
-          </NextLink>
-          <button className="w-full flex items-center justify-center gap-2 h-10 bg-red-600 drop-shadow-xl hover:bg-red-400 transition-all rounded-md text-white px-12">
+          </Link>
+          <Link
+            href="/appointments"
+            className="w-full flex items-center justify-center gap-2 h-10 bg-red-600 drop-shadow-xl hover:bg-red-400 transition-all rounded-md text-white px-12"
+          >
             <Image src={list} alt="file icon" /> See Appointments
-          </button>
+          </Link>
         </article>
       </article>
     </Layout>
