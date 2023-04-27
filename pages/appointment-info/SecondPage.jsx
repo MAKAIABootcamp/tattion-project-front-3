ba55ab6a0c2a9eb9329c098e5e1f038572992eae
Andrés Vélez <87771860+andresdvelez@users.noreply.github.com>
2023-04-27 09:13:27 -0500

    fix:general-styles

--- pages/appointment-info/SecondPage.jsx
+++ pages/appointment-info/SecondPage.jsx
@@ -9,70 +9,78 @@ import profileImg from "@/public/appointment/profileImg.svg";
 import check from "@/public/appointment/check.svg";
 import { FaArrowLeft } from "react-icons/fa";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 
 const divVariants = {
-    hidden: { opacity: 0, y: -50 },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            type: "spring",
-            stiffness: 500,
-            damping: 30,
-        },
+  hidden: { opacity: 0, y: -50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+      damping: 30,
     },
+  },
 };
 
 const SecondPage = ({ setPage }) => {
-    const handleBack = () => {
-        setPage(1);
-    };
+  const handleBack = () => {
+    setPage(1);
+  };
 
-    return (
-        <motion.div>
-            <motion.div
-                variants={divVariants}
-                initial="hidden"
-                animate="visible"
-                className="w-[400px] h-[600px] bg-gray-black absolute top-0 left-0 bottom-0 right-0 m-auto rounded-md heroImg flex flex-col gap-8 items-center py-12"
-            >
-                <FaArrowLeft
-                    onClick={handleBack}
-                    className="text-white text-2xl absolute left-7 cursor-pointer hover:text-red-600 "
-                    size={24}
-                />
-                <div className="flex flex-col gap-8 items-center mt-12">
-                    <h1 className=" m-auto text-white font-montserrat font-semibold">
-                        {" "}
-                        Your Appintment
-                    </h1>
-                    <Image src={check} />
-                </div>
+  const { quotation } = useSelector((state) => state.quote);
 
-                <form className="w-full p-5 flex flex-col gap-9">
-                    <div className="flex flex-col gap-6">
-                        <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex items-center gap-3">
-                            <BsCalendar3 size={14} />
-                            <p>21/12</p>
-                        </div>
-                        <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex items-center gap-3">
-                            <HiLocationMarker size={14} />
-                            <p>Location</p>
-                        </div>
-                        <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex justify-between items-center gap-3">
-                            <p>Artist</p>
-                            <Image src={profileImg} />
-                        </div>
-                    </div>
-                    <Link href="/payment">
-                        <button className="w-full h-10 bg-red-600 drop-shadow-xl rounded-md text-white px-6">
-                            Pay
-                        </button>
-                    </Link>
-                </form>
-            </motion.div>
-        </motion.div>
-    );
+  return (
+    <motion.div>
+      <motion.div
+        variants={divVariants}
+        initial="hidden"
+        animate="visible"
+        className="w-[400px] h-[600px] bg-gray-black absolute top-0 left-0 bottom-0 right-0 m-auto rounded-md heroImg flex flex-col gap-8 items-center py-12"
+      >
+        <FaArrowLeft
+          onClick={handleBack}
+          className="text-white text-2xl absolute left-7 cursor-pointer hover:text-red-600 "
+          size={24}
+        />
+        <div className="flex flex-col gap-8 items-center mt-12">
+          <h1 className=" m-auto text-white font-montserrat font-semibold">
+            {" "}
+            Your Appintment
+          </h1>
+          <Image src={check} />
+        </div>
+
+        <form className="w-full p-5 flex flex-col gap-9">
+          <div className="flex flex-col gap-6">
+            <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex items-center gap-3">
+              <BsCalendar3 size={14} />
+              <p>21/12</p>
+            </div>
+            <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex items-center gap-3">
+              <HiLocationMarker size={14} />
+              <p>Llano Grande, Rionegro, Antioquia</p>
+            </div>
+            <div className="w-full h-10 rounded-md bg-[#2b2c2c] drop-shadow-xl text-white px-6 flex justify-between items-center gap-3">
+              <p>{quotation.artist.name}</p>
+              <Image
+                src={quotation.artist.img}
+                width={32}
+                height={32}
+                className="rounded-md pointer-events-none"
+              />
+            </div>
+          </div>
+          <Link href="/payment">
+            <button className="w-full h-10 bg-red-600 drop-shadow-xl rounded-md text-white px-6">
+              Pay
+            </button>
+          </Link>
+        </form>
+      </motion.div>
+    </motion.div>
+  );
 };
 
 export default SecondPage;
--- pages/welcome/index.jsx
+++ pages/welcome/index.jsx
@@ -2,32 +2,40 @@ import React from "react";
 import Image from "next/image";
 import Layout from "@/layouts/MainLayout";
 import Link from "next/link";
-import { useAuth } from "@/context/AuthContext";
+import Cookies from "js-cookie";
 
 //assets
 import logo from "@/public/landingPage/logo.png";
 import file from "@/public/welcome/file-icon.svg";
 import list from "@/public/welcome/list-icon.svg";
+import { BiLogOut } from "react-icons/bi";
 
 const Welcome = () => {
-  const { currentUser } = useAuth();
-
   return (
     <Layout>
       <article className="w-[670px] h-[400px] bg-gray-black absolute top-0 left-0 bottom-0 right-0 m-auto rounded-md heroImg flex justify-around gap-8 items-center">
         <Image src={logo} alt="Logo" width={300} height={280} />
 
         <article className="flex flex-col items-center gap-6 mr-4">
-          <Link href="/designs">
-            <button className="w-full flex items-center justify-center gap-2 h-10 bg-red-600 hover:bg-red-400 transition-all drop-shadow-xl rounded-md text-white px-12">
-              <Image src={file} alt="file icon" /> Quote Tattoo
+          <Link href="/designs" className="w-full">
+            <button className="w-full flex items-center justify-between gap-2 h-10 bg-red-600 hover:bg-red-400 transition-all drop-shadow-xl rounded-md text-white px-12">
+              <Image src={file} alt="file icon" />{" "}
+              <span className="right">Quote Tattoo</span>
             </button>
           </Link>
           <Link
             href="/appointments"
-            className="w-full flex items-center justify-center gap-2 h-10 bg-red-600 drop-shadow-xl hover:bg-red-400 transition-all rounded-md text-white px-12"
+            className="w-full flex items-center justify-between gap-2 h-10 bg-red-600 drop-shadow-xl hover:bg-red-400 transition-all rounded-md text-white px-12"
           >
             <Image src={list} alt="file icon" /> See Appointments
+          </Link>
+          <Link
+            href="/"
+            onClick={() => Cookies.remove("loggedin")}
+            className="w-full flex items-center justify-between gap-2 h-10 bg-red-600 drop-shadow-xl hover:bg-red-400 transition-all rounded-md text-white px-12"
+          >
+            <BiLogOut className="text-xl font-bold " />{" "}
+            <span className="right text-center">Log Out</span>
           </Link>
         </article>
       </article>
